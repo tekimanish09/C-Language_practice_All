f305fa0dedfbba5f9fd1fb06410261854e6590c1
TEKI MANISH <tekimanish10@gmail.com>
2025-12-31 03:07:51 +0530

    Add ./Chapter 4/Quiz_Question.c

---
+++ Chapter 4/Quiz_Question.c
@@ -0,0 +1,16 @@
+// Code for hard coded value >_<
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i <= 1000)
+    {
+        if (i >= 900)
+        {
+            printf("The value if i is %d \n", i);
+        }
+        i++;
+    }
+    return 0;
+}
