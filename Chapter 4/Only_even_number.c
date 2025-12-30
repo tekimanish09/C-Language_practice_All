88254cb4395661d934390ab83efa3bdb5ba47006
TEKI MANISH <tekimanish10@gmail.com>
2025-12-31 02:53:33 +0530

    Add ./Chapter 4/Only_even_number.c

---
+++ Chapter 4/Only_even_number.c
@@ -0,0 +1,16 @@
+// Code for hard coded value >_<
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i <= 100)
+    {
+        if (i%2==0)
+        {
+            printf("The value if i is %d \n", i);
+        }
+        i++;
+    }
+    return 0;
+}
