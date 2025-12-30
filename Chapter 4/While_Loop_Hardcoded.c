519b3597b28fc13f7c7c9601c12ebde5f8fc3526
TEKI MANISH <tekimanish10@gmail.com>
2025-12-31 02:34:10 +0530

    Add ./Chapter 4/While_Loop_Hardcoded.c

---
+++ Chapter 4/While_Loop_Hardcoded.c
@@ -0,0 +1,13 @@
+// Code for hard coded value >_<
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i < 5)
+    {
+        printf("You can do it Manish!!! %d \n", i);
+        i = i + 1;
+    }
+    return 0;
+}
