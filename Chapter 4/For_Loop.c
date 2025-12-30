abc72dedc0e8b4cf38a0418f9c24a4e647786d03
TEKI MANISH <tekimanish10@gmail.com>
2025-12-31 03:54:32 +0530

    Add ./Chapter 4/For_Loop.c

---
+++ Chapter 4/For_Loop.c
@@ -0,0 +1,11 @@
+// Code for hard coded value >_<
+
+#include <stdio.h>
+
+int main(){
+    int n=69;
+    for(int i=0;i<=n;i++){
+        printf("The value of 'i' is %d \n",i);
+    }
+    return 0;
+}
