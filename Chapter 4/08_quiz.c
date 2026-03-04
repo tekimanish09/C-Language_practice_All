7cf86c7921adf268f01fbca92f7840b865147805
TEKI MANISH <tekimanish10@gmail.com>
2026-03-04 18:21:34 +0530

    Add ./Chapter 4/08_quiz.c

---
+++ Chapter 4/08_quiz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n, i=1;
+    printf("Enter the 'n' number:");
+    scanf("%d",&n);
+    do{
+        printf("%d \n",i);
+        i++;
+    }while(i<=n);
+    return 0;
+}
