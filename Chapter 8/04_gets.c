fd11e501bc5776a4702368ffeb70b172ac888497
TEKI MANISH <tekimanish10@gmail.com>
2026-03-15 14:26:30 +0530

    Add ./Chapter 8/04_gets.c

---
+++ Chapter 8/04_gets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #include <string.h>
+
+int main()
+{
+    char str[100];
+    puts("Enter your sentence: ");
+    gets(str);
+    puts(str);
+    // printf("%d", strlen(str));
+
+    return 0;
+}
