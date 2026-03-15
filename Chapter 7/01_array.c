fa77ac15b2945012a3429e675b40569e02489727
TEKI MANISH <tekimanish10@gmail.com>
2026-03-15 12:51:38 +0530

    Add ./Chapter 7/01_array.c

---
+++ Chapter 7/01_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int marks[90];
+    marks[0]=65;
+    marks[1]=82;
+    printf("The marks in 0 and 1 is %d and %d respectively", marks[0],marks[1]);
+    return 0;
+}
+
+// for(int i =0;i<90;i++){
+//     printf("Enter the value for index %d: ",i);
+//     scanf("%d",marks[i]);
+// }
+
+// printf("Enter the index size: \n");
+// scanf("%d",&num);
