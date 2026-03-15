ea3fd87152ad000748ba8edb127a9fb7c72195f3
TEKI MANISH <tekimanish10@gmail.com>
2026-03-15 14:02:04 +0530

    Add ./Chapter 8/02_printing.c

---
+++ Chapter 8/02_printing.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "MANISH";
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     printf("%c", name[i]);
+    // }
+    printf("%s",name);
+    return 0;
+}
